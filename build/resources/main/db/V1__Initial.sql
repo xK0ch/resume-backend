b38f774bee127716eac58a0ee269afd62cd8fbe8
xK0ch <mail@fynn-koch.de>
2024-07-28 16:00:34 +0200

    Remove stuff

--- build/resources/main/db/V1__Initial.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE contact_information
-(
-    id               UUID PRIMARY KEY         NOT NULL DEFAULT gen_random_uuid(),
-    created_at       TIMESTAMP WITH TIME ZONE NOT NULL,
-    last_modified_at TIMESTAMP WITH TIME ZONE NOT NULL,
-
-    first_name       VARCHAR(255)             NOT NULL,
-    last_name        VARCHAR(255)             NOT NULL
-);
