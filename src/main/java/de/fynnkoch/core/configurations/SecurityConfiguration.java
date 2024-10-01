fc897306bc9810164116070b9598ef4d7953291a
xK0ch <mail@fynn-koch.de>
2024-10-01 05:42:47 +0200

    Add cors configuration

--- src/main/java/de/fynnkoch/core/configurations/SecurityConfiguration.java
+++ src/main/java/de/fynnkoch/core/configurations/SecurityConfiguration.java
@@ -2,13 +2,13 @@ package de.fynnkoch.core.configurations;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -30,7 +33,7 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) throws Exception {
     httpSecurity
-        .cors(AbstractHttpConfigurer::disable)
+        .cors(withDefaults())
         .csrf((csrf) -> csrf.ignoringRequestMatchers("/**"))
         .authorizeHttpRequests(
             (requests) ->
@@ -47,6 +50,19 @@ public class SecurityConfiguration {
                     .hasRole("ADMIN"))
         .httpBasic(withDefaults());
     return httpSecurity.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    final CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://fynn-koch.de"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
+    configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+    configuration.setAllowCredentials(true);
+
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   @Bean
